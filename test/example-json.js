7b0a7ed59fcbc7384309fb7cb001b477bf805fef
Rob Ellis <rob@chloi.io>
2014-04-01 20:32:54 -0700

    Adds JSON interface for creating a sentence

--- test/example-json.js
+++ test/example-json.js
@@ -1,9 +1,17 @@
-// Construct a tree from a text string
+// Construct a tree from a JSON string
 
 var earley = require("../earley").earley;
 var grammar = GrammerFromFile("../sample.cfg")
 
-var sentence = earley.Sentence.SentenceFromString("time/time<N> flies/flies<N>/flies<V> like/like<P>/like<V> an/an<D> arrow/arrow<N>");
+var json = [
+		{ "word":"time", "lemma":"time", "pos": "N"},
+		{ "word":"flies", "lemma":"flies", "pos": ["N","V"] },
+		{ "word":"like", "lemma":"like", "pos": ["V","P"]},
+		{ "word":"an", "lemma":"an", "pos": "D"},
+		{ "word":"arrow", "lemma":"arrow", "pos": "N"}
+	]
+
+var sentence = earley.Sentence.SentenceFromJSON(json);
 var parser = new earley.Parser(grammar, sentence, false)
 var result = parser.parse();
 
@@ -11,7 +19,6 @@ if (parser.is_valid_sentence()) {
 	var trees = new earley.ParseTrees(result);
 	console.log("Sentence is Valid");
 	console.log(trees.toString());
-	
 } else {
 	console.log("Invalid Sentence");
 }
